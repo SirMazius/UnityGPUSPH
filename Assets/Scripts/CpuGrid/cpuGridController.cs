3a424bff546b0b95cd1a82264aafc0baaf1a3ea2
SirMazius <matias-garcia@outlook.com>
2021-02-08 19:22:10 +0100

    Commit inicial empezamos a trabajar en la tabla hash

---
+++ Assets/Scripts/CpuGrid/cpuGridController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cpuGridController : MonoBehaviour
+{
+    private int[] gridCounter; // Almacena el numero de particulas por celda.
+    public List<Vector3> particlePositions; // Posiciones de las particulas;
+    public float cellSize;
+    public float countX, countY, countZ;
+    void Start()
+    {
+        gridCounter = new int[100];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //InitGrid();
+        DrawGrid();
+    }
+
+    private void InitGrid()
+    {
+        gridCounter.Initialize();
+    }
+
+    private int getCellIndex(Vector3 particlePosition)
+    {
+        float x = Mathf.Floor(particlePosition.x) / cellSize;
+        float y = Mathf.Floor(particlePosition.y) / cellSize;
+        float z = Mathf.Floor(particlePosition.z) / cellSize;
+        return 1;
+        //return y * countX + x
+    }
+
+    public void DrawGrid()
+    {
+        for (int j = 0; j < countY; j++)
+        {
+            for (int i = 0; i <= countX; i++) // pinta las celdas en el eje X.
+            {
+                Debug.DrawLine(new Vector3(i * cellSize, j * cellSize, 0.0f), new Vector3(i * cellSize, j * cellSize, cellSize * countZ), Color.red);
+            }
+
+            for (int i = 0; i <= countZ; i++) // Dibuja las celdas en el eje Z.
+            {
+                Debug.DrawLine(new Vector3(0.0f, j * cellSize, i * cellSize), new Vector3(countX * cellSize, j * cellSize, i * cellSize), Color.blue);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TestingDispatcher.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TestingDispatcher : MonoBehaviour
+{
+    public ComputeShader computeShader;
+    public ComputeShader computeShader2;
+    public ComputeShader hashTableShader;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ComputeBuffer floatsBuffer = new ComputeBuffer(64, 4); // Array de 64 floats (4 bytes por float).
+        List<float> floatsData = new List<float>(new float[64]);
+        float[] outputFloatsData = new float[64];
+        // List<float> floatsData = Enumerable.Range(0, 64).Select(i => 0.0f).ToList();
+        //foreach (float f in floatsData)
+        //{
+        //    Debug.Log(f);
+        //}
+
+        floatsBuffer.SetData(floatsData);
+
+        int kernelIndex = computeShader.FindKernel("CSMain");
+        int kernelIndex2 = computeShader.FindKernel("CSSecond");
+        int kernelIndex3 = computeShader2.FindKernel("CSThird");
+
+        
+
+        computeShader.SetBuffer(kernelIndex, "floatsData", floatsBuffer);
+        computeShader.SetBuffer(kernelIndex2, "floatsData", floatsBuffer);
+        computeShader2.SetBuffer(kernelIndex3, "floatsData", floatsBuffer);
+
+        computeShader.Dispatch(kernelIndex, 1, 1, 1);
+        computeShader.Dispatch(kernelIndex2, 1, 1, 1);
+        computeShader2.Dispatch(kernelIndex3, 1, 1, 1);
+
+        floatsBuffer.GetData(outputFloatsData);
+        foreach (float f in outputFloatsData)
+        {
+            Debug.Log(f);
+        }
+
+        floatsBuffer.Dispose();
+    }
+
+}
